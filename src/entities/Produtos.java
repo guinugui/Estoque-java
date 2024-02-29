2beb5cd0282884c75d71df22881e301b0a3dd5ea
Guilherme Ribeiro <guilhermevendaseprodutos@gmail.com>
2024-02-29 12:58:49 -0300

    Classe produto criada

---
+++ src/entities/Produtos.java
@@ -0,0 +1,49 @@
+package entities;
+
+public class Produtos {
+
+	private String nome;
+	private Double preco;
+	private Integer quantidade;
+
+	public Produtos(String nome, Double preco, Integer quantidade) {
+		this.nome = nome;
+		this.preco = preco;
+		this.quantidade = quantidade;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public double totalEmEstoque() {
+		return getQuantidade();
+	}
+	public void addProdutos(Integer quantidade) {
+		
+	}
+	public void removeProdutos(Integer quantidade) {
+		
+	}
+
+}
